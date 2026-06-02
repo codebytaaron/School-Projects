27d33a9baf3c455652c206596561e898f16c7a6d
Code By T. Aaron <167483502+codebytaaron@users.noreply.github.com>
2026-06-02 08:45:44 -0400

    Delete simple_interest_calculator.py

--- simple_interest_calculator.py
+++
@@ -1,9 +0,0 @@
-principal = float(input("Enter the principal amount: $"))
-rate = float(input("Enter the annual interest rate (%): "))
-time = float(input("Enter the time in years: "))
-
-interest = (principal * rate * time) / 100
-total_amount = principal + interest
-
-print("\nSimple Interest: $", round(interest, 2))
-print("Total Amount: $", round(total_amount, 2))
